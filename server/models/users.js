37df2ec7dcbddc63d5866be933e77f7cfa1ae81e
gurmehar1553 <gurmehar1553.be20@chitkara.edu.in>
2023-07-06 19:54:46 +0530

    initial commit

---
+++ server/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    FirstName:{
+        type:String,
+        require:true
+    },
+    LastName:{
+        type:String,
+        require:true
+    },
+    Email:{
+        type:String,
+        require:true
+    },
+    Password:{
+        type:String,
+        require:true
+    },
+    isAdmin:{
+        type:Boolean
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+module.exports=User
